1ae0ebd9363e0667447efe463ebd00ac674fa76b
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2021-11-09 08:44:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -625,16 +625,35 @@ async def auto_filter(client, msg, spoll=False):
     imdb = await get_poster(search) if IMDB else None
     if imdb:
         cap = IMDB_TEMPLATE.format(
-          <b>🏷 Title</b>: <a href={url}>{title}</a>
-          🎭 Genres: {genres}
-          📆 Year: <a href={url}/releaseinfo>{year}</a>
-          🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-          ☀️ Languages : <code>{languages}</code>
-          👥 Cast : <code>{cast}</code>
-          📀 RunTime: {runtime} Minutes
-          📆 Release Info : {release_date}
-          🎛 Countries : <code>{countries}</code>
-          👨‍🦱Developer: @Arjunbots
+                      query = search,
+            title = imdb['title'],
+            votes = imdb['votes'],
+            aka = imdb["aka"],
+            seasons = imdb["seasons"],
+            box_office = imdb['box_office'],
+            localized_title = imdb['localized_title'],
+            kind = imdb['kind'],
+            imdb_id = imdb["imdb_id"],
+            cast = imdb["cast"],
+            runtime = imdb["runtime"],
+            countries = imdb["countries"],
+            certificates = imdb["certificates"],
+            languages = imdb["languages"],
+            director = imdb["director"],
+            writer = imdb["writer"],
+            producer = imdb["producer"],
+            composer = imdb["composer"],
+            cinematographer = imdb["cinematographer"],
+            music_team = imdb["music_team"],
+            distributors = imdb["distributors"],
+            release_date = imdb['release_date'],
+            year = imdb['year'],
+            genres = imdb['genres'],
+            poster = imdb['poster'],
+            plot = imdb['plot'],
+            rating = imdb['rating'],
+            url = imdb['url']ntries}</code>
+            IMDB_TEMPLATE
 
         )
     else:
